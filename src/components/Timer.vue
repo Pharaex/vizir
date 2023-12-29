48b161ebcce544d02cd8e6a39c5eb81bd66b8bb9
Pharaex <ab.abdalla@hotmail.com>
2023-12-29 17:03:51 +0400

    feat: Timer-Type-and-reactivity

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -18,6 +18,20 @@
     </template>
     <template #footer>
       <div>
+        <fwb-radio
+          v-model="timerTypePicked"
+          name="radio"
+          label="Work"
+          value="one"
+        />
+        <fwb-radio
+          v-model="timerTypePicked"
+          name="radio"
+          label="Meditation"
+          value="two"
+        />
+      </div>
+      <div>
         <fwb-button @click="closeModal" color="alternative"> Set </fwb-button>
       </div>
     </template>
@@ -26,13 +40,28 @@
 
 <script setup lang="ts">
 import { ref, computed, watch, onBeforeUnmount } from "vue";
-import { FwbButton, FwbModal, FwbInput } from "flowbite-vue";
+import { FwbButton, FwbModal, FwbInput, FwbRadio } from "flowbite-vue";
 const hours = ref<number>(0);
 const minutes = ref<number>(30);
 const seconds = ref<number>(0);
 const timerActive = ref(false);
 const isShowModal = ref(false);
 const timerType = ref("Time To Work");
+const timerTypePicked = ref("one");
+
+watch(timerTypePicked, (newVal) => {
+  if (newVal === "two") {
+    timerType.value = "Time To Meditate";
+    hours.value = 0;
+    minutes.value = 5;
+    seconds.value = 0;
+  } else if (newVal === "one") {
+    timerType.value = "Time To Work";
+    hours.value = 0;
+    minutes.value = 30;
+    seconds.value = 0;
+  }
+});
 
 const totalSeconds = computed(
   () => hours.value * 3600 + minutes.value * 60 + seconds.value
@@ -84,15 +113,29 @@ const updateTimerParts = () => {
 };
 
 const updateTimerType = () => {
-  if (timerType.value === "Time To Work") {
+  if (
+    timerType.value === "Time To Work" ||
+    timerType.value === "Time To Meditate"
+  ) {
     timerType.value = "Break Time";
     hours.value = 0;
     minutes.value = 5;
     seconds.value = 0;
-  } else {
+  } else if (
+    timerType.value === "Break Time" &&
+    timerTypePicked.value === "one"
+  ) {
     timerType.value = "Time To Work";
     hours.value = 0;
     minutes.value = 30;
+    seconds.value = 0;
+  } else if (
+    timerType.value === "Break Time" &&
+    timerTypePicked.value === "two"
+  ) {
+    timerType.value = "Time To Meditate";
+    hours.value = 0;
+    minutes.value = 5;
     seconds.value = 0;
   }
 };
